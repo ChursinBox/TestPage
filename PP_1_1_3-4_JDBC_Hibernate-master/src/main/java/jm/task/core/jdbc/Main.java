787bb0c4884913eb68e7743a9a7e39e6bfbbe67a
ChursinBox <164941336+ChursinBox@users.noreply.github.com>
2024-07-17 08:02:05 +0400

    first commit

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -6,27 +6,39 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
+        UserService us = new UserServiceImpl();
 
-        UserService userService = new UserServiceImpl();
+//        us.dropUsersTable();
+//        us.createUsersTable();
+//        us.saveUser("Vladimir", "Lenin", (byte) 55);
+//        us.saveUser("Vladimir", "Lenin", (byte) 55);
+//        us.saveUser("Vladimir", "Lenin", (byte) 55);
+//        us.saveUser("Vladimir", "Lenin", (byte) 55);
 
+//        us.removeUserById(1);
 
-        userService.dropUsersTable();
+        System.out.println(us.getAllUsers());
 
-        userService.createUsersTable();
-
-        userService.saveUser("Vladimir", "Lenin", (byte) 55);
-        userService.saveUser("Nikolai", "Vtoroy", (byte) 58);
-        userService.saveUser("Pop", "King", (byte) 25);
-        userService.saveUser("Harry", "Potter", (byte) 15);
-
-        userService.getAllUsers();
-
-        userService.removeUserById(3);
-        userService.getAllUsers();
-
-        userService.cleanUsersTable();
-
-        Util.closeConnection();
+//        UserService userService = new UserServiceImpl();
+//
+//
+//        userService.dropUsersTable();
+//
+//        userService.createUsersTable();
+//
+//        userService.saveUser("Vladimir", "Lenin", (byte) 55);
+//        userService.saveUser("Nikolai", "Vtoroy", (byte) 58);
+//        userService.saveUser("Pop", "King", (byte) 25);
+//        userService.saveUser("Harry", "Potter", (byte) 15);
+//
+//        userService.getAllUsers();
+//
+//        userService.removeUserById(3);
+//        userService.getAllUsers();
+//
+//        userService.cleanUsersTable();
+//
+//        Util.closeConnection();
 
     }
 }
--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,41 +1,116 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoHibernateImpl implements UserDao {
+public class UserDaoHibernateImpl extends Util implements UserDao {
+
+    private final SessionFactory factory = getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
 
     @Override
     public void createUsersTable() {
-
+        Transaction transaction = null;
+        try(Session session = getSession()) {
+            transaction = openTransaction();
+            session.createQuery("CREATE TABLE `katah`.`users` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `name` VARCHAR(45) NOT NULL,\n" +
+                    "  `lastname` VARCHAR(45) NOT NULL,\n" +
+                    "  `age` INT NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`));");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction transaction = null;
+        try(Session session = getSession()) {
+            transaction = openTransaction();
+            session.createQuery("DROP TABLE IF EXISTS katah");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
 
+        try(Session session = getSession()) {
+            transaction = openTransaction();
+            User user = new User(name,lastName,age);
+            session.save(user);
+            closeTransactionSession();
+        } catch(Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try(Session session = getSession()) {
+            transaction = openTransaction();
+            User user = session.get(User.class, id);
+            session.remove(user);
+            closeTransactionSession();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        List<User> users = new ArrayList<>();
+        try (Session session = getSession()) {
+            transaction = openTransaction();
+            users = session.createCriteria(User.class).list();
+            closeTransactionSession();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try(Session session = getSession()) {
+            transaction = openTransaction();
+            session.createQuery("DELETE FROM katah");
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
